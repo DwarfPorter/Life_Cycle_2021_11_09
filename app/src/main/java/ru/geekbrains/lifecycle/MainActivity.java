d7ee45e2e3e5387bb63bf9712e919bcf97e7dbe0
DwarfPorter <mrvlmor@gmail.com>
2021-11-09 21:00:56 +0300

    переопределение методов callback

--- app/src/main/java/ru/geekbrains/lifecycle/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lifecycle/MainActivity.java
@@ -3,9 +3,12 @@ package ru.geekbrains.lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "[LifeCycle]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,59 @@ public class MainActivity extends AppCompatActivity {
         } else {
             firstTimeRun = "Повторный запуск";
         }
-        Toast.makeText(this, firstTimeRun + " - onCreate()", Toast.LENGTH_SHORT).show();
+        makeToast(firstTimeRun + " - onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        makeToast("onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        makeToast("onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy()");
+    }
+
+    private void makeToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, message);
     }
 }
