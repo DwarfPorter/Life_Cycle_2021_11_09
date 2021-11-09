e685e653fafa4b327a4e8606e1497300641af45a
DwarfPorter <mrvlmor@gmail.com>
2021-11-09 20:37:36 +0300

    переопределение onCreate

--- app/src/main/java/ru/geekbrains/lifecycle/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package ru.geekbrains.lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String firstTimeRun;
+        if (savedInstanceState == null){
+            firstTimeRun = "Первый запуск";
+        } else {
+            firstTimeRun = "Повторный запуск";
+        }
+        Toast.makeText(this, firstTimeRun + " - onCreate()", Toast.LENGTH_SHORT).show();
     }
 }
